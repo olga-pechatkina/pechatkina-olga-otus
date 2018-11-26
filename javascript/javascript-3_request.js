52608ca71d90a148f4a0ad61ca36da7a00e4b2b3
Pechatkina Olga <aaangel@inbox.ru>
2018-11-26 16:44:24 +0300

    changes

--- javascript/javascript-3_request.js
+++ javascript/javascript-3_request.js
@@ -2,12 +2,6 @@ const http = require('http')
 
 function request(N, type) {
     if (type == 0) {
-<<<<<<< Updated upstream
-        for (var i = 0; i < N; i++) {
-            var promise = new Promise(resolve => {
-                http.get('http://127.0.0.1:3000');
-            })
-=======
                 http.get('http://127.0.0.1:3000', (res) => {
                     if(res!== undefined)
                     {
@@ -16,7 +10,6 @@ function request(N, type) {
                     }
         }    
                 );
->>>>>>> Stashed changes
         }
     if (type == 1) {
         for (var i = 0; i < N; i++) {
@@ -29,8 +22,4 @@ const args = process.argv.slice(2);
 var N = args[0];
 var type = args[1];
 
-<<<<<<< Updated upstream
 request(N, type);
-=======
-request(N, type);
->>>>>>> Stashed changes
