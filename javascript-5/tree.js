245ec4f7964b73b0978fb24a4180f4e9044a7402
Pechatkina Olga <aaangel@inbox.ru>
2018-12-12 08:50:28 +0300

    requested changes

--- javascript-5/tree.js
+++ javascript-5/tree.js
@@ -2,29 +2,34 @@ const fs = require('fs')
 const path = require('path')
 const folderPath = process.argv.slice(2).toString();
 
-var findPath = function(folderPath,result,cb) {
-    fs.readdir(folderPath, function(err, items) {
- if(items !=undefined) {
-    items.forEach(function(item, i, arr)
-    {
-      const newPath = path.resolve(folderPath,items[i]);
-        let stats = fs.stat (newPath, function (err, stats)
-        {
-        if (stats.isDirectory() == true)
-        {
-            result.dirs.push(newPath);
-            findPath(newPath,result,cb);
-        }
-            else if (items[i] !== undefined){
-            result.files.push(newPath);
-            }
+var findPath = function (folderPath, result, cb) {
+    fs.readdir(folderPath, function (err, items) {
+        if (err) return cb(err);
+        let itemsLeft = items.length;
+        items.forEach(function (item, i, arr) {
+            const newPath = path.resolve(folderPath, items[i]);
+            let stats = fs.stat(newPath, function (err, stats) {
+                if (stats.isDirectory() == true) {
+                    findPath(newPath, result, function (err, res) {
+                        result.dirs.push(newPath);
+                        if (!--itemsLeft) {
+                            cb(null, result);
+                        }
+                    });
+                } else {
+                    result.files.push(newPath);
+                    if (!--itemsLeft) {
+
+                        cb(null, result);
+                    }
+                }
+            })
         })
+        if (!itemsLeft) {
+            return cb(null, result);
+        }
+
     })
-       if (items == "") {
-        return cb(result);
-       }
-    }
-})
 }
 
 var files = {
@@ -32,10 +37,8 @@ var files = {
     files: []
 };
 
-findPath(folderPath, files, function(v) {
-     files =v; 
-     var files_json = JSON.stringify(files);
-     console.log(files_json);
-    });
-
-
+findPath(folderPath, files, function (err, res) {
+    files = res;
+    var files_json = JSON.stringify(files);
+    console.log(files_json);
+});
