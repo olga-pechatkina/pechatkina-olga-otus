5ac965d2ef533ac91e750afdb20f1c467d15686a
Pechatkina Olga <aaangel@inbox.ru>
2019-01-27 13:15:37 +0300

    realized

---
+++ javascript-13/customTree.js
@@ -0,0 +1,98 @@
+class myTree extends HTMLElement {
+  constructor() {
+    super();
+    var id = 0;
+    var items = '';
+  }
+}
+
+class myLeaf extends HTMLElement {
+  constructor() {
+    super();
+    var id = 0;
+  }
+}
+
+customElements.define('my-tree', myTree);
+customElements.define('my-leaf', myLeaf);
+
+var div = document.createElement('div');
+div.setAttribute('data-json', '{"id": 1,"items": [{"id": 2,"items": [{ "id": 3 }]}]}');
+document.body.appendChild(div);
+var shadowRoot = div.attachShadow({
+  mode: 'open'
+});
+class myTree extends HTMLElement {
+  constructor() {
+    super();
+    var id = 0;
+    var items = '';
+  }
+}
+
+class myLeaf extends HTMLElement {
+  constructor() {
+    super();
+    var id = 0;
+  }
+}
+
+customElements.define('my-tree', myTree);
+customElements.define('my-leaf', myLeaf);
+
+var div = document.createElement('div');
+div.setAttribute('data-json', '{"id": 1,"items": [{"id": 2,"items": [{ "id": 3 }]}]}');
+document.body.appendChild(div);
+var shadowRoot = div.attachShadow({
+  mode: 'open'
+});
+
+function findNext(div) {
+  var treeElements = JSON.parse(div.dataset.json);
+  if (Object.keys(treeElements).length === 0) {
+    return shadowRoot;
+  }
+
+  if (treeElements["id"]) {
+    if (treeElements["items"]) {
+      const localTree = document.createElement('my-tree');
+      localTree.id = treeElements["id"];
+      localTree.items = treeElements["items"][0];
+      localTree.dataset.json = JSON.stringify(localTree.items);
+      shadowRoot.appendChild(localTree);
+      findNext(localTree);
+    } else {
+      const localLeaf = document.createElement('my-leaf');
+      localLeaf.id = treeElements["id"];
+      delete treeElements.id;
+      localLeaf.dataset.json = JSON.stringify(treeElements);
+      shadowRoot.appendChild(localLeaf);
+      findNext(localLeaf);
+    }
+  }
+}
+findNext(div);
+
+function findNext(div) {
+  var treeElements = JSON.parse(div.dataset.json);
+  if (Object.keys(treeElements).length === 0) {
+    return shadowRoot;
+  }
+
+  if (treeElements["id"]) {
+    if (treeElements["items"]) {
+      const localTree = document.createElement('my-tree');
+      localTree.id = treeElements["id"];
+      localTree.items = treeElements["items"][0];
+      localTree.dataset.json = JSON.stringify(localTree.items);
+      shadowRoot.appendChild(localTree);
+      findNext(localTree);
+    } else {
+      const localLeaf = document.createElement('my-leaf');
+      shadowRoot.appendChild(localLeaf);
+      localLeaf.id = treeElements["id"];
+    }
+  }
+}
+
+findNext(div);
